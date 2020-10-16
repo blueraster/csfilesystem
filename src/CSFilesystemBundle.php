17cc360d25d111d31b926d9d00038e99567b8c20
Sean Bethel <tech@americanmadeweb.com>
2020-10-15 21:42:10 -0400

    updates

--- src/CSFilesystemBundle.php
+++ src/CSFilesystemBundle.php
@@ -6,4 +6,16 @@ use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class CSFilesystemBundle extends Bundle
 {
+	public function __construct(){
+		$js = '<script>
+	(function(d){
+		var menu = d.getElementById("side-menu");
+		var li = d.createElement("li");
+		li.innerHTML = \'<a href="/files" class="active"><i class="fa fa-folder-o fa-fw"></i> List Files</a>\';
+		menu.appendChild(li);
+	})(document);
+</script>';
+		echo $js;
+	}
 }
+
